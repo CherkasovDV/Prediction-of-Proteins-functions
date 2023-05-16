f14f85683cee4348153f9f02c1c068cb3923f798
makenastya <makeevaa@biocad.ru>
2023-05-16 21:38:37 +0300

    Предобработка данных

--- project-itog.py
+++ project-itog.py
@@ -1,1 +1,52 @@
 #Project final version
+import numpy as np
+import pandas as pd
+from collections import Counter
+from keras.preprocessing import text, sequence
+from keras.preprocessing.text import Tokenizer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from keras.models import Model
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.layers import Concatenate
+from tensorflow.keras.layers import Dense,Dot,Conv1D,MaxPooling1D,Activation,Dropout,LSTM,Flatten,GlobalMaxPooling1D,Input
+import matplotlib.pyplot as plt
+import seaborn as sns
+#считываем данные (из pdb)
+seq = pd.read_csv('pdb_data_seq.csv')
+data = pd.read_csv('pdb_data_no_dups.csv')
+df = data.merge(seq,how='inner',on='structureId').drop_duplicates(['structureId'])
+#предобработка
+print(df.isnull().sum())
+df = df.dropna()
+df = df[df['macromoleculeType_x'] == 'Protein']
+df = df.reset_index()
+df = df.drop(['index'],axis=1)
+print(df.residueCount_x.describe())
+df = df.loc[df.residueCount_x<1200]
+df['merge_col'] = list(map(lambda i : str(i['sequence']) + ' ' + str(i['residueCount_y'])+" "+str(i['phValue'])+" "+str(i['densityPercentSol'])+" "+str(i['macromoleculeType_y']) + " " + str(i['residueCount_x']) + " " + str(i['resolution']) + " " + str(i['crystallizationTempK']) + " " + str(i['chainId']) ,df.to_dict(orient="records") ))
+#количество в каждом классе
+k = Counter(df.classification)
+top = 10
+sort = k.most_common()[:top]
+classes = [a[0] for a in sort]
+count = [a[1] for a in sort]
+print("минимум " + str(count[-1]) + " образцов в каждом классе")
+#добавляем в датафрейм
+df = df[[a in classes for a in df.classification]]
+seqs = df['merge_col']
+length = [len(s) for s in seqs]
+print(df.classification.value_counts())
+
+lab = LabelBinarizer()
+a = lab.fit_transform(df.classification)
+X=df.drop('classification',axis=1)
+df.iloc[0]['merge_col']
+max_len = max(length)
+tokenizer = Tokenizer(char_level=True)
+tokenizer.fit_on_texts(seqs)
+X_seq = tokenizer.texts_to_sequences(seqs)
+X_seq = tf.keras.utils.pad_sequences(X_seq, maxlen=max_len) #проверяем что риды одной длины
+print(df.info())
