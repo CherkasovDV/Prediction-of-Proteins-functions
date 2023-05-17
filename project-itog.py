52b56cd2fdb2972acfcd0e28cf938fa43c8b752b
CherkasovDV <cherk.dan@mail.ru>
2023-05-17 14:05:45 +0300

    Обучение и тестирование
    Co-Authored-By: k0nstantinova <113921392+k0nstantinova@users.noreply.github.com>

--- project-itog.py
+++ project-itog.py
@@ -12,6 +12,10 @@ from keras.models import Model
 from tensorflow.keras.layers import Embedding
 from tensorflow.keras.layers import Concatenate
 from tensorflow.keras.layers import Dense,Dot,Conv1D,MaxPooling1D,Activation,Dropout,LSTM,Flatten,GlobalMaxPooling1D,Input
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import ModelCheckpoint
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 #считываем данные (из pdb)
@@ -62,3 +66,48 @@ mem_layer = LSTM(150)(concat_layer) #слой LST-памяти
 
 output = Dense(top, activation='softmax')(mem_layer)
 model = Model(inputs=input, outputs=output) #Модель, группирующая слои в объект с функциями обучения/вывода
+
+#Обучение и тестирование
+opt = Adam(learning_rate=0.001)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['acc'])
+print(model.summary())
+X_seq = np.array(X_seq)
+X_seq = np.array(list(X_seq))
+b = np.random.rand(len(df)) < 0.8
+
+X_seq_train = X_seq[b]
+a_train = a[b]
+
+X_seq_test = X_seq[~b]
+a_test = a[~b]
+
+early_stop = EarlyStopping(monitor='val_loss',patience=5)
+
+#Сохранение результатов по разным параметрам модели
+model_save = ModelCheckpoint(
+    '/working/best_model.h5', monitor='val_acc', verbose=1, save_best_only=True,
+    save_weights_only=False, mode='auto')
+
+#история
+h = model.fit(x=X_seq_train, y=a_train, batch_size=128, epochs=15, verbose=1, validation_split=0.2,callbacks=[model_save])
+
+best_model = tf.keras.models.load_model('/working/best_model.h5')
+
+a_test_0 = best_model.predict(X_seq_test) #предсказание
+confusion_Matrix = confusion_matrix(np.argmax(a_test,axis=1), np.argmax(a_test_0,axis=1))
+
+# Построение нормализованной матрицы
+confusion_Matrix = confusion_Matrix.astype('float') / confusion_Matrix.sum(axis=1)[:, np.newaxis]
+np.set_printoptions(precision=2)
+plt.figure(figsize=(10,10))
+plt.imshow(confusion_Matrix, interpolation='nearest', cmap=plt.cm.Reds)
+plt.title('Confusion matrix')
+plt.colorbar()
+ticks = np.arange(top)
+plt.xticks(ticks, sort, rotation=90)
+plt.yticks(ticks, sort)
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+print(confusion_Matrix)
